19e89239c961d6778b1f8171e2be453100506038
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-11-06 14:31:12 -0700

    feat: add MessageChannel#receive action

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -6,4 +6,8 @@ class MessageChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def receive(data)
+  ActionCable.server.broadcast('message', data)
+  end
 end
