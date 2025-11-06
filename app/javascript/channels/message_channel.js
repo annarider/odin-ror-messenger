31c1dfbb50ecc35bf6ab291efcbd4695830b6c35
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-11-06 10:47:03 -0700

    feat: add EventListener handle when message submit

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -13,3 +13,18 @@ const messageChannel = consumer.subscriptions.create("MessageChannel", {
     // Called when there's incoming data on the websocket for this channel
   }
 });
+
+document.addEventListener("turbo:load", () => {
+  let form = document.querySelector('#message-form')
+  if(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      let messageInput = document.querySelector('#message-input').value
+      if(messageInput == '') return;
+      const message = {
+        body: messageInput
+      }
+      messageChannel.send({message: message})
+    })
+  }
+})
